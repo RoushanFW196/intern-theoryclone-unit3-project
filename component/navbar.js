a687cdf3fe7bbff09c12366203b7f1aa16b55953
Madhurima956 <madhurimabanerjee97@gmail.com>
2021-11-07 11:55:06 +0530

    navbar using import export

---
+++ component/navbar.js
@@ -0,0 +1,12 @@
+function navbar(){
+
+    return  `
+
+    <img id="hamburger" src="https://image.flaticon.com/icons/png/512/1828/1828859.png"  />
+    <img id="logo"  src="https://assets.interntheory.com/creative/logo.png" />
+    <button><a href="login_student.html">SIGN IN</a></button>
+     <img id="cart_logo" src="https://image.flaticon.com/icons/png/512/4297/4297127.png"  />`
+
+}
+
+export default navbar ;
