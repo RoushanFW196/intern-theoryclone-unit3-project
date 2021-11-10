47ba2fe5a773a8202954e13bd302d5ae6812e535
RoushanFW196 <roushankumar6338@gmail.com>
2021-11-10 15:12:05 +0530

    adding contactus pages

--- component/navbar.js
+++ component/navbar.js
@@ -2,7 +2,7 @@ function navbar(){
 
     return  `
 
-    <img id="hamburger" class="btn" src="https://image.flaticon.com/icons/png/512/1828/1828859.png"  />
+    <img id="hamburger"  src="https://image.flaticon.com/icons/png/512/1828/1828859.png"  />
     <img id="navbar_intern_logo"  src="https://assets.interntheory.com/creative/logo.png" />
     <button id="signin"><a href="loginstudent.html">SIGN IN</a></button>
     <a id="cart_logo" href="cart.html">
