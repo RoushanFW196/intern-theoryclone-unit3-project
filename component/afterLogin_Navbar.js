b3093c7d57faa4f01ef2f728de72f68d9355f53e
Madhurima956 <madhurimabanerjee97@gmail.com>
2021-11-10 13:54:33 +0530

    after login navbar js file

---
+++ component/afterLogin_Navbar.js
@@ -0,0 +1,28 @@
+function afterLogin_navbar(){
+
+    return  `
+
+    <div id="left_div">
+        <a href=""><div id="humburger" ><img class="btn" src="https://static.thenounproject.com/png/703781-200.png"  /></div>
+        </a>
+        <a href=""><div><img id="navbar_intern_logo"  src="https://assets.interntheory.com/creative/logo.png" /></div></a>
+        
+    </div>
+
+    <div id="right_div">
+        <a href=""><div  id="notify_icon"><img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/notification-1614268-1368970.png" alt="">
+        </div></a>
+
+        <a href="cart.html"><div id="cart_logo" > <img src="https://iconape.com/wp-content/png_logo_vector/shopping-cart.png"  >
+        </div> </a>
+
+        <a  href=""><div id="user_icon"><img src="https://www.sunsetlearning.com/wp-content/uploads/2019/09/User-Icon-Grey-300x300.png" alt="">
+        </div> </a>
+            
+    </div> `
+
+     
+
+}
+
+export default  afterLogin_navbar ;
