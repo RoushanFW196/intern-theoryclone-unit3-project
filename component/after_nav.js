6dcfff6401dd98b930b34731e0877a0a384e7f01
Madhurima956 <madhurimabanerjee97@gmail.com>
2021-11-12 00:17:07 +0530

    after login navbar component

---
+++ component/after_nav.js
@@ -0,0 +1,120 @@
+
+function afternav(){
+
+    return ` <nav id="afterLogin_navbar">
+    <div id="left_div" >
+        <div id="humburger" ><img class='btn' src="https://static.thenounproject.com/png/703781-200.png"  /></div>
+        <img id="navbar_intern_logo" src="https://assets.interntheory.com/creative/logo.png"/>
+    
+        
+            <div class="sidebar">
+                
+                <div id="left_div">
+                    <div id="humburger" ><img class='btn' src="https://static.thenounproject.com/png/703781-200.png"  /></div>
+                
+                    <a href=""><div><img id="navbar_intern_logo"  src="https://assets.interntheory.com/creative/logo.png" /></div></a>
+                    
+                </div>
+        
+                <div class="a">
+                    <div class="b">
+                        <img src="https://assets.interntheory.com/creative/default-images/girlProfile.jpg">
+                    </div>
+                </div>
+            <ul>
+                <li><a href="#"><i class="fas fa-th-large"></i><b>Dashboard</b></a></li>
+            <li><a href="#"><i class="fas fa-user-circle"></i><b>Profile</b></a></li>
+            <li><a href="#"><i class="fas fa-list"></i><b>Internships</b></a></li>
+            <li><a href="#"><i class="fas fa-business-time"></i><b>Jobs</b></a></li>
+            <li> <a href="#" ><i class="fas fa-file"></i><b>Courses</b></a></li>
+            <li><a href="#"><i class="fas fa-dollar-sign"></i><b>Transactions</b></a></li>
+            <li><a href="#"><i class="fas fa-phone-alt"></i><b>Contact us</b></a></li>
+            <li><a href="#"><i class="fas fa-folder"></i><b>Start Your Business Today</b></a></li>
+            <div class="m"></div>
+            <li><a href="#" > <i class="fas fa-lock"></i><b>Change Password</b></a></li>
+            <li><a href="#" ><i class="fas fa-arrow-circle-right"></i><b>logout</b></a></li>
+        </ul>  
+    </div>
+</div>
+<div id="right_div">
+    <a href=""><div  id="notify_icon"><img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/notification-1614268-1368970.png" alt="">
+    </div></a>
+
+    <a href="cart.html"><div id="cart_logo" > <img src="https://iconape.com/wp-content/png_logo_vector/shopping-cart.png"  >
+    </div> </a>
+
+    <a  href=""><div id="user_icon"><img src="https://www.sunsetlearning.com/wp-content/uploads/2019/09/User-Icon-Grey-300x300.png" alt="">
+    </div> </a>
+        
+</div> 
+</nav>`
+
+}
+
+function footer123(){
+    return `  <div id="content">
+    <div id="upper_part">
+        <div id="footer_image">
+            <img src="https://image.flaticon.com/icons/png/512/220/220200.png" />
+            <img src="https://image.flaticon.com/icons/png/512/1409/1409945.png" />
+            <img src="https://image.flaticon.com/icons/png/512/124/124021.png" />
+            <img src="https://image.flaticon.com/icons/png/512/2111/2111463.png" />
+        </div>
+        <div>
+            <ul>
+                <li class="footer_text">BY PLACE</li>
+                <hr>
+                <li class="footer_text"><a href="#">Internships In Mumbai</a></li>
+                <li class="footer_text"><a href="#">Internships In Delhi</a></li>
+                <li class="footer_text"><a href="#">Internships In Bangalore</a></li>
+                <li class="footer_text"><a href="#">Internships In Pune</a></li>
+                <li class="footer_text"><a href="#">Internships In Hyderabad</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li class="footer_text">BY PROFILE</li>
+                <hr>
+                <li class="footer_text"><a href="#">Marketing Internships</a></li>
+                <li class="footer_text"><a href="#">Business Development Internships</a></li>
+                <li class="footer_text"><a href="#">Content Writing Internships</a></li>
+                <li class="footer_text"><a href="#">Graphic Designing Internships</a></li>
+                <li class="footer_text"><a href="#">HR Internships</a></li>
+                <li class="footer_text"><a href="#">Engineering Internships</a></li>
+
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li class="footer_text">BY TYPE</li>
+                <hr>
+                <li class="footer_text"><a href="#">Full Time Internships</a></li>
+                <li class="footer_text"><a href="#">Part Time Internships</a></li>
+                <li class="footer_text"><a href="#">Work From Home Internships</a></li>
+
+            </ul>
+        </div>
+    </div>
+    <div id="lower_part">
+
+        <div id="detail">
+            <div><a href="#">FIND INTERNSHIP</a></div>
+            <div><a href="#">POST INTERNSHIP</a>
+            </div>
+            <div><a href="#">COURSES</a></div>
+            <div><a href="#">TERMS AND CONDITIONS</a></div>
+            <div><a href="#">PRIVACY POLICY</a></div>
+            <div><a href="#">CONTACT US</a></div>
+            <div><a href="#">ABOUT US</a></div>
+            <div><a href="#">SITEMAP</a></div>
+
+        </div>
+        <div id="year">
+            <div><a href="#">© 2015 - 2021 INTERNTHEORY. ALL RIGHTS RESERVED.</a></div>
+        </div>
+    </div>
+</div>`
+}
+
+
+export {afternav,footer123};
