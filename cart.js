01efffcab95ecc456e05e822b82a117524059e89
Madhurima956 <madhurimabanerjee97@gmail.com>
2021-11-11 19:38:02 +0530

    commit

--- cart.js
+++ cart.js
@@ -13,7 +13,7 @@ function showproduct() {
 
         let delBtn = document.createElement("button");
         delBtn.textContent = "X";
-        delBtn.setAttribute("id","cutitem");
+        delBtn.setAttribute("id", "cutitem");
         delBtn.onclick = function () {
             deleteFromCart(products);
         }
@@ -118,40 +118,80 @@ function applycode() {
     }
 }
 
-// modal js
+
+// start card details modal js
 
 var modal = document.getElementById("myModal");
+
+
 var btn = document.getElementById("myBtn");
+
+
 var span = document.getElementsByClassName("close")[0];
+
 btn.onclick = function () {
     modal.style.display = "block";
 }
+
+
 span.onclick = function () {
     modal.style.display = "none";
 }
+
+
 function validate() {
-   
 
-    var cardnumber = document.getElementById("cardNumber").value;
-    var expiry = document.getElementById("exp").value;
-    var cardname = document.getElementById("cardName").value;
-    var Cvv = document.getElementById("cvv").value;
+    // modal js
 
-   // console.log(cardnumber, expiry, cardname, Cvv);
-
-    if (cardnumber.length == 16 && expiry.length == 4 && cardname != "" && Cvv.length == 3) {
-        alert("payment Successful");
-
-        setTimeout(function () {
-            window.location.href = "index.html";
-        }, 2000);
-        // window.localStorage.clear();
-        localStorage.removeItem('coursecart')
-    } else {
-        alert("your card is details wrong")
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+    span.onclick = function () {
+        modal.style.display = "none";
     }
 }
+    function validat() {
 
-var payable_money=document.getElementById("money");
- 
-payable_money.innerHTML="₹"+JSON.parse(localStorage.getItem("total"));
+
+        var cardnumber = document.getElementById("cardNumber").value;
+        var expiry = document.getElementById("exp").value;
+        var cardname = document.getElementById("cardName").value;
+        var Cvv = document.getElementById("cvv").value;
+
+
+        // console.log(cardnumber, expiry, cardname, Cvv);
+
+        // console.log(cardnumber, expiry, cardname, Cvv);
+
+
+        if (cardnumber.length == 16 && expiry.length == 4 && cardname != "" && Cvv.length == 3) {
+            alert("payment Successful");
+
+            setTimeout(function () {
+
+
+
+                window.location.href = "index.html";
+
+            }, 2000);
+            // window.localStorage.clear();
+            localStorage.removeItem('coursecart')
+        } else {
+            alert("your card is details wrong")
+        }
+
+
+    }
+
+
+    // end card details modal js
+
+}
+
+var payable_money = document.getElementById("money");
+
+payable_money.innerHTML = "₹" + JSON.parse(localStorage.getItem("total"));
+
